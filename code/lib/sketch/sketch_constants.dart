813db6bc1fd179505b8684b03cbb53f49f63596a
RidmalChamod <e21065@eng.pdn.ac.lk>
2026-09-15 06:06:45 +0530

    fixs - wall size to 150mm

--- code/lib/sketch/sketch_constants.dart
+++ code/lib/sketch/sketch_constants.dart
@@ -16,7 +16,7 @@ const double pointSnapRadiusScreen = 30.0;
 const double pointSelectRadiusScreen = 22.0;
 const double snapThresholdDeg = 3.0;
 const double minAngleDistance = 10.0;
-const double wallThickness = 20.0;
+const double wallThickness = 30.0;
 
 List<Offset> thickWallRect(Offset a, Offset b, double thickness) {
   final dx = b.dx - a.dx;
